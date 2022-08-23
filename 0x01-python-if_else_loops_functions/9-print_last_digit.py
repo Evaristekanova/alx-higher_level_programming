85824e830d819f0c39d55ab662790fd9afdd2799
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-08-23 13:05:17 -0700

    loops anf functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/bin/bash/python3
 def print_last_digit(number):
-	if number < 0:
-		remainder = number % -10
-	else:
-		remainder = number % 10
-	return remainder
+    if number < 0:
+        number = ((-1) * number) % 10
+    else:
+        number = number % 10
+
+    print("{:d}".format(number), end="")
+    return number
