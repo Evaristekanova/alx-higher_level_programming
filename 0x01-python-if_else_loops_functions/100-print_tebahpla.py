be34994c7fd9eb67b26ca1730c49596c7d9fab0b
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-08-23 14:07:47 -0700

    loops and functions

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+res=''
+for i, val in enumerate(range(ord('z'), ord('a') - 1, -1), start=1):
+    if(i % 2 == 0):
+        res += chr(val - 32)
+    else:
+        res += chr(val)
+print(res, end="")
