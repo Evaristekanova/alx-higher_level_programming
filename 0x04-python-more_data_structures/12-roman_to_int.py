c19e68f1dd902337c43a9cfd52376b7e3c8721f7
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-08-31 16:14:01 -0700

    dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
-def value(r):
-    if(r == "I"):
-        return 1
-    if(r == "V"):
-        return 5
-    if(r == "X"):
-        return 10
-    if(r == "L"):
-        return 50
-    if(r == "C"):
-        return 100
-    if(r == "D"):
-        return 500
-    if(r == "M"):
-        return 1000
-    return -1
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
 
-def roman_to_int(rom_str):
-    result = 0
-    idx = 0
-    leng = len(rom_str)
-    while(idx < len(rom_str)):
-        f = value(rom_str[idx])
-        if(idx + 1 < len(rom_str)):
-            f2 = value(rom_str[idx + 1])
-            if(f >= f2):
-                result += f
-                idx += 1
-            else:
-                result += f2 - f
-                idx += 2
-        else:
-            result += f
-            idx += 1
-    return result
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
