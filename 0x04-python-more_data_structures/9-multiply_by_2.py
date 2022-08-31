8fcbedaa15a67d9adf0295b1ddfb5916d7ed9c68
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-08-31 13:54:04 -0700

    dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
-#1/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    for idx, val in sorted(a_dictionary.items()):
-        print("{} : {}".format(idx, val * 2))
+#!/usr/bin/python3
+def multiply_by_2(my_dict):
+    tmp_dict = my_dict.copy()
+    for x in tmp_dict.keys():
+        tmp_dict[x] *= 2
+    return (tmp_dict)
